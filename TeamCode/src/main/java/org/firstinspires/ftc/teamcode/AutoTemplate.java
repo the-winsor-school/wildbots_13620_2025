0d1ade1ecdd59828738906c7800b314641c9c613
kenna26 <90514808+kenna26@users.noreply.github.com>
2024-10-04 18:50:44 -0400

    fixing inital commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTemplate.java
@@ -3,11 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.Arm.Claw;
-import org.firstinspires.ftc.teamcode.Arm.FullArm;
-import org.firstinspires.ftc.teamcode.Arm.MotorState;
-import org.firstinspires.ftc.teamcode.AutonLibrary.AllAutonMovements;
-
 @Autonomous(name = "Close Park (red or blue)")
 public class AutoTemplate extends LinearOpMode {
 
