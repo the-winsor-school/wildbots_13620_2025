254cc1db644da2489cc0527dde7bd873f93daa5d
anayaraikar <anaya.raikar@winsor.edu>
2024-10-05 10:38:49 -0400

    started claw code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+//package org.firstinspires.ftc.teamcode.Arm;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+
+public class Claw {
+
+    public CRServo right;
+    public CRServo left;
+
+    public Claw(CRServo rightServo, CRServo leftServo) {
+        right = rightServo;
+        left = leftServo;
+
+    //moves claw asynchronously
+
+    
+
+}
