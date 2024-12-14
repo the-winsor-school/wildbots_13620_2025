2220286cc6e7e35cee6f170ef96c5458679b57db
kenna26 <90514808+kenna26@users.noreply.github.com>
2024-12-14 10:44:13 -0500

    controller testing code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingAuton.java
@@ -3,8 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@Autonomous(name = "testing")
+@TeleOp(name = "testing")
 public class TestingAuton extends LinearOpMode {
 
     Robot robot;
@@ -12,18 +13,31 @@ public class TestingAuton extends LinearOpMode {
     LinearOpMode opMode;
 
     public void runOpMode() throws InterruptedException {
-        robot = new Robot(this);
 
         waitForStart();
 
         while (opModeIsActive()) {
 
-            telemetry.addData("Back Distance:", robot.backDistance.getDistance());
-            telemetry.addData("Right Distance:", robot.rightDistance.getDistance());
-            telemetry.addData("Left Distance:", robot.leftDistance.getDistance());
-
-            telemetry.addData("Right Color Red:", robot.rightColor.getRed());
-            telemetry.addData("Left Color Red:", robot.rightColor.getRed());
+            telemetry.addLine("_______Gampepad1________");
+            telemetry.addData("right x:", gamepad1.right_stick_x);
+            telemetry.addData("right y:", gamepad1.right_stick_y);
+            telemetry.addData("left x:", gamepad1.left_stick_x);
+            telemetry.addData("left y:", gamepad1.left_stick_y);
+            telemetry.addLine();
+            telemetry.addData("a:", gamepad1.a);
+            telemetry.addData("b:", gamepad1.b);
+            telemetry.addData("x:", gamepad1.x);
+            telemetry.addData("y:", gamepad1.y);
+            telemetry.addLine();
+            telemetry.addData("dpad up:", gamepad1.dpad_up);
+            telemetry.addData("dpad down:", gamepad1.dpad_down);
+            telemetry.addData("dpad left:", gamepad1.dpad_left);
+            telemetry.addData("dpad right:", gamepad1.dpad_right);
+            telemetry.addLine();
+            telemetry.addData("right bumper:", gamepad1.right_bumper);
+            telemetry.addData("left bumper:", gamepad1.left_bumper);
+            telemetry.addData("right trigger:", gamepad1.right_trigger);
+            telemetry.addData("left trigger:", gamepad1.left_trigger);
 
             telemetry.update();
         }
